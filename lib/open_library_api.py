453823bc91409a0bce0997a7e2fb5a8a930df7ba
Anna Kioko <annakioko00@gmail.com>
2024-04-07 08:21:28 +0300

    completed assignmnet

--- lib/open_library_api.py
+++ lib/open_library_api.py
@@ -45,7 +45,20 @@ class Search:
         return response_formatted
 
 
-# results = Search().get_search_results()
+    def get_search_results_json(self):
+     search_term = "the lord of the rings"
+
+     search_term_formatted = search_term.replace(" ", "+")
+     fields = ["title", "author_name"]
+     fields_formatted = ",".join(fields)
+     limit = 1
+
+     URL = f"https://openlibrary.org/search.json?title={search_term_formatted}&fields={fields_formatted}&limit={limit}"
+     print(URL)
+     response = requests.get(URL)
+     return response.json()
+    
+# results = Search().get_search_results()    
 # print(results)
 
 # results_json = Search().get_search_results_json()
